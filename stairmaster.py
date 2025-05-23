816dab12f215630f768b5393f3aedd3a93e1188c
Ellie Alavi <elhamalavi.f@gmail.com>
2025-05-23 15:06:36 -0400

    Dynamic Fibonacci

--- stairmaster.py
+++ stairmaster.py
@@ -20,11 +20,29 @@
 # 1. 1 step + 1 step + 1 step
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
- 
-def climb_stairs(n):
-    pass
+
+
+# basicalli it is Fibonacci series:
+
+def climbStairs(n, memo = None):
+        
+        if n == 0 or n == 1:
+            return 1
+
+        memo = memo if memo is not None else {} 
+
+        if n in memo:
+            return memo[n]
+        
+        memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo)
+        
+        return memo[n]
+
 
     # at each step, how many choices do we have?
     # given each choice, how many combinations of steps are left?
     # is there a threshold below which we have only a limited number of combinations?
     # what threshold, and how many combinations?
+
+
+print(climbStairs(30))
--- test_stairmaster.py
+++ test_stairmaster.py
@@ -1,25 +1,25 @@
-from stairmaster import climb_stairs
+from stairmaster import climbStairs
 
 def test_1_step():
-    assert climb_stairs(1) == 1
+    assert climbStairs(1) == 1
 
 def test_2_step():
-    assert climb_stairs(2) == 2
+    assert climbStairs(2) == 2
 
 def test_3_step():
-    assert climb_stairs(3) == 3
+    assert climbStairs(3) == 3
 
 def test_10_step():
-    assert climb_stairs(10) == 89
+    assert climbStairs(10) == 89
 
 def test_30_step():
-    assert climb_stairs(30) == 1346269
+    assert climbStairs(30) == 1346269
 
 def test_35_step():
-    assert climb_stairs(35) == 14930352
+    assert climbStairs(35) == 14930352
 
-# def test_40_step():
-#     assert climb_stairs(40) == 165580141
+def test_40_step():
+    assert climbStairs(40) == 165580141
 
-# def test_50_step():
-#     assert climb_stairs(50) == 20365011074
+def test_50_step():
+    assert climbStairs(50) == 20365011074
